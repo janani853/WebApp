54c81cf10930329afca49a84435c1ee4a60c1a32
janani853 <58103962+janani853@users.noreply.github.com>
2020-07-16 08:56:35 +0530

    Update acat.java

--- Acceptancetest/src/test/java/acceptancetest/acat.java
+++ Acceptancetest/src/test/java/acceptancetest/acat.java
@@ -12,8 +12,8 @@ public class acat {
 	public HtmlUnitDriver driver;
 	//public WebDriver driver;
 	public WebDriverWait wait;
-	public String URL = "http://18.216.15.102/ProdWebapp";
-	public String URL1 = "http://18.216.15.102/ProdWebapp/avncreatepage.jsp";
+	public String URL = "http://18.219.113.152/ProdWebapp";
+	public String URL1 = "http://18.219.113.152/ProdWebapp/avncreatepage.jsp";
 	//public String URL = "http://localhost:8080/AVNCommunication-1.0/avnlogin.jsp";
 	//public String URL1 = "http://localhost:8080/AVNCommunication-1.0/avncreatepage.jsp";
 	
